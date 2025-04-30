23986a583029294e9359aff5f824514be805f3f7
William Boles <wibosco@users.noreply.github.com>
2025-04-30 14:21:23 +0100

    Implemented 'WeSplit' example

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by William Boles on 25/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

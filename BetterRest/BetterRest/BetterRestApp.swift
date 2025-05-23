bf39934daedeba033cba71d4fcd625abc787e9c4
William Boles <wibosco@users.noreply.github.com>
2025-05-23 16:00:27 +0100

    Implemented 'BetterRest' example

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by William Boles on 22/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

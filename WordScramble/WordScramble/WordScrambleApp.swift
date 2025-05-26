ce6f347202524e90d3b3ac76761cfb05df89a3e2
William Boles <wibosco@users.noreply.github.com>
2025-05-26 12:27:16 +0100

    Implemented 'WordScramble' example

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by William Boles on 26/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
